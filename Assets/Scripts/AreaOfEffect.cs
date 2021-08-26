6db3447b6f4171ece008b979b2b6c0d3280946bf
toasted226 <51060273+toasted226@users.noreply.github.com>
2021-08-26 11:24:03 +0200

    Added AOE

--- Assets/Scripts/AreaOfEffect.cs
+++ Assets/Scripts/AreaOfEffect.cs
@@ -4,13 +4,15 @@ public class AreaOfEffect : MonoBehaviour
 {
     public float targetSize;
     public float growthMultiplier;
+    public float shrinkMultiplier;
     public float acceleration;
-    public float growthBoundary;
+    public float boundary;
 
     private float m_StartingSize;
     private Animator m_Animator;
     private CircleCollider2D m_Collider;
     private bool m_IsExpanding = true;
+    private bool m_IsShrinking;
 
     private void Start() 
     {
@@ -27,11 +29,37 @@ public class AreaOfEffect : MonoBehaviour
             float change = transform.localScale.x + Time.deltaTime * growthMultiplier;
             transform.localScale = new Vector3(change, change, transform.localScale.z);
 
-            if(targetSize - transform.localScale.x <= growthBoundary) 
+            if(targetSize - transform.localScale.x <= boundary) 
             {
                 m_IsExpanding = false;
+                m_Collider.enabled = true;
                 m_Animator.SetTrigger("explode");
             }
         }
+
+        if(m_IsShrinking) 
+        {
+            shrinkMultiplier += Time.deltaTime * acceleration;
+            float change = transform.localScale.x - Time.deltaTime * shrinkMultiplier;
+            transform.localScale = new Vector3(change, change, transform.localScale.z);
+
+            if(transform.localScale.x <= boundary) 
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player")) 
+        {
+            other.GetComponent<PlayerController>().TakeDamage(1);
+        }
+    }
+
+    public void StartShrink() 
+    {
+        m_IsShrinking = true;
     }
 }
