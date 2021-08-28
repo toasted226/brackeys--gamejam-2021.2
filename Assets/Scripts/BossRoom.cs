2514320eb1bbc55172f8e49d0b183a7a78babe05
toasted226 <51060273+toasted226@users.noreply.github.com>
2021-08-28 16:48:41 +0200

    Added boss music implementation

--- Assets/Scripts/BossRoom.cs
+++ Assets/Scripts/BossRoom.cs
@@ -10,17 +10,21 @@ public class BossRoom : MonoBehaviour
     public GameObject healthbar;
 
     private CameraFollow m_CameraFollow;
+    private AudioSource m_AudioSource;
     private bool m_PlayedCinematic;
 
     private void Start() 
     {
         m_CameraFollow = Camera.main.transform.GetComponentInParent<CameraFollow>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player")) 
         {
+            //Why do I hear boss music?
+            m_AudioSource.Play();
             StartCoroutine(EntranceCinematic());
         }
     }
